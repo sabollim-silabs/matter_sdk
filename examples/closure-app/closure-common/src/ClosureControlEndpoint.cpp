ce14e2206f4b2e08db88a631e213ce4dc4db73a7
Satya Naag Bollimpalli <sabollim@silabs.com>
2025-06-17 01:15:08 +0530

    test static analysis

--- examples/closure-app/closure-common/src/ClosureControlEndpoint.cpp
+++ examples/closure-app/closure-common/src/ClosureControlEndpoint.cpp
@@ -21,97 +21,98 @@
 #include <app-common/zap-generated/cluster-objects.h>
 #include <protocols/interaction_model/StatusCode.h>
 
-using namespace chip;
-using namespace chip::app::Clusters::ClosureControl;
-
-using Protocols::InteractionModel::Status;
-
-namespace {
-constexpr ElapsedS kDefaultCountdownTime = 30;
-} // namespace
-
-Status PrintOnlyDelegate::HandleCalibrateCommand()
-{
-    ChipLogProgress(AppServer, "HandleCalibrateCommand");
-    // Add the calibration logic here
-    return Status::Success;
-}
-
-Status PrintOnlyDelegate::HandleMoveToCommand(const Optional<TargetPositionEnum> & position, const Optional<bool> & latch,
-                                              const Optional<Globals::ThreeLevelAutoEnum> & speed)
-{
-    ChipLogProgress(AppServer, "HandleMoveToCommand");
-    // Add the move to logic here
-    return Status::Success;
-}
-
-Status PrintOnlyDelegate::HandleStopCommand()
-{
-    ChipLogProgress(AppServer, "HandleStopCommand");
-    // Add the stop logic here
-    return Status::Success;
-}
-
-CHIP_ERROR PrintOnlyDelegate::GetCurrentErrorAtIndex(size_t index, ClosureErrorEnum & closureError)
-{
-    // This function should return the current error at the specified index.
-    // For now, we dont have a ErrorList implemented, so will return CHIP_ERROR_PROVIDER_LIST_EXHAUSTED.
-    return CHIP_ERROR_PROVIDER_LIST_EXHAUSTED;
-}
-
-bool PrintOnlyDelegate::IsReadyToMove()
-{
-    // This function should return true if the closure is ready to move.
-    // For now, we will return true.
-    return true;
-}
-
-bool PrintOnlyDelegate::IsManualLatchingNeeded()
-{
-    // This function should return true if manual latching is needed.
-    // For now, we will return false.
-    return false;
-}
-
-ElapsedS PrintOnlyDelegate::GetCalibrationCountdownTime()
-{
-    // This function should return the calibration countdown time.
-    // For now, we will return kDefaultCountdownTime.
-    return kDefaultCountdownTime;
-}
-
-ElapsedS PrintOnlyDelegate::GetMovingCountdownTime()
-{
-    // This function should return the moving countdown time.
-    // For now, we will return kDefaultCountdownTime.
-    return kDefaultCountdownTime;
-}
-
-ElapsedS PrintOnlyDelegate::GetWaitingForMotionCountdownTime()
-{
-    // This function should return the waiting for motion countdown time.
-    // For now, we will return kDefaultCountdownTime.
-    return kDefaultCountdownTime;
-}
-
-CHIP_ERROR ClosureControlEndpoint::Init()
-{
-    ClusterConformance conformance;
-    conformance.FeatureMap()
-        .Set(Feature::kPositioning)
-        .Set(Feature::kMotionLatching)
-        .Set(Feature::kSpeed)
-        .Set(Feature::kVentilation)
-        .Set(Feature::kPedestrian)
-        .Set(Feature::kCalibration)
-        .Set(Feature::kProtection)
-        .Set(Feature::kManuallyOperable);
-    conformance.OptionalAttributes().Set(OptionalAttributeEnum::kCountdownTime);
-
-    ClusterInitParameters initParams;
-
-    ReturnErrorOnFailure(mLogic.Init(conformance, initParams));
-    ReturnErrorOnFailure(mInterface.Init());
-
-    return CHIP_NO_ERROR;
-}
+ using namespace chip;
+ using namespace chip::app::Clusters::ClosureControl;
+ 
+ using Protocols::InteractionModel::Status;
+ 
+ namespace {
+ constexpr ElapsedS kDefaultCountdownTime = 30;
+ } // namespace
+ 
+ Status PrintOnlyDelegate::HandleCalibrateCommand()
+ {
+     ChipLogProgress(AppServer, "HandleCalibrateCommand");
+     // Add the calibration logic here
+     return Status::Success;
+ }
+ 
+ Status PrintOnlyDelegate::HandleMoveToCommand(const Optional<TargetPositionEnum> & position, const Optional<bool> & latch,
+                                               const Optional<Globals::ThreeLevelAutoEnum> & speed)
+ {
+     ChipLogProgress(AppServer, "HandleMoveToCommand");
+     // Add the move to logic here
+     return Status::Success;
+ }
+ 
+ Status PrintOnlyDelegate::HandleStopCommand()
+ {
+     ChipLogProgress(AppServer, "HandleStopCommand");
+     // Add the stop logic here
+     return Status::Success;
+ }
+ 
+ CHIP_ERROR PrintOnlyDelegate::GetCurrentErrorAtIndex(size_t index, ClosureErrorEnum & closureError)
+ {
+     // This function should return the current error at the specified index.
+     // For now, we dont have a ErrorList implemented, so will return CHIP_ERROR_PROVIDER_LIST_EXHAUSTED.
+     return CHIP_ERROR_PROVIDER_LIST_EXHAUSTED;
+ }
+ 
+ bool PrintOnlyDelegate::IsReadyToMove()
+ {
+     // This function should return true if the closure is ready to move.
+     // For now, we will return true.
+     return true;
+ }
+ 
+ bool PrintOnlyDelegate::IsManualLatchingNeeded()
+ {
+     // This function should return true if manual latching is needed.
+     // For now, we will return false.
+     return false;
+ }
+ 
+ ElapsedS PrintOnlyDelegate::GetCalibrationCountdownTime()
+ {
+     // This function should return the calibration countdown time.
+     // For now, we will return kDefaultCountdownTime.
+     return kDefaultCountdownTime;
+ }
+ 
+ ElapsedS PrintOnlyDelegate::GetMovingCountdownTime()
+ {
+     // This function should return the moving countdown time.
+     // For now, we will return kDefaultCountdownTime.
+     return kDefaultCountdownTime;
+ }
+ 
+ ElapsedS PrintOnlyDelegate::GetWaitingForMotionCountdownTime()
+ {
+     // This function should return the waiting for motion countdown time.
+     // For now, we will return kDefaultCountdownTime.
+     return kDefaultCountdownTime;
+ }
+ 
+ CHIP_ERROR ClosureControlEndpoint::Init()
+ {
+     ClusterConformance conformance;
+     conformance.FeatureMap()
+         .Set(Feature::kPositioning)
+         .Set(Feature::kMotionLatching)
+         .Set(Feature::kSpeed)
+         .Set(Feature::kVentilation)
+         .Set(Feature::kPedestrian)
+         .Set(Feature::kCalibration)
+         .Set(Feature::kProtection)
+         .Set(Feature::kManuallyOperable);
+     conformance.OptionalAttributes().Set(OptionalAttributeEnum::kCountdownTime);
+ 
+     ClusterInitParameters initParams;
+ 
+     ReturnErrorOnFailure(mLogic.Init(conformance, initParams));
+     ReturnErrorOnFailure(mInterface.Init());
+ 
+     return CHIP_NO_ERROR;
+ }
+ 
